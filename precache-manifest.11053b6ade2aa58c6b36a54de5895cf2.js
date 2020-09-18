c156d5972c3570ac0155527d558b454e008267ee
Nathália Constantino Costa <n.athiccosta@gmail.com>
2020-09-18 10:01:18 -0300

    Updates

---
+++ precache-manifest.11053b6ade2aa58c6b36a54de5895cf2.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6d5c28efeb0d5b7e866bee445662941",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "855034d0af9b66a4e87b",
+    "url": "/portfolio/static/css/main.b12540c2.chunk.css"
+  },
+  {
+    "revision": "3823101b2a95a0eed833",
+    "url": "/portfolio/static/js/2.12617618.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/portfolio/static/js/2.12617618.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "855034d0af9b66a4e87b",
+    "url": "/portfolio/static/js/main.027042e0.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "972f04d509aded68932b4b4e7b1e4733",
+    "url": "/portfolio/static/media/group-3@2x.972f04d5.png"
+  },
+  {
+    "revision": "d059cb88ec2cc5f3ed88068cffc2e132",
+    "url": "/portfolio/static/media/imac@2x.d059cb88.png"
+  },
+  {
+    "revision": "74dfdc2410d6f520208cb9c8114e28b1",
+    "url": "/portfolio/static/media/imgLonga.74dfdc24.jpg"
+  },
+  {
+    "revision": "bef095bcd23a1a27ffde008966048aa3",
+    "url": "/portfolio/static/media/macbook_2x-.bef095bc.png"
+  }
+]);
